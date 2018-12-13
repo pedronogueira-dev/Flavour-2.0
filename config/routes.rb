6f59a117648ed3b5de5d5946f3dca6620df48fc4
Pedro Nogueira <17pedro.n@gmail.com>
2018-12-13 18:38:35 +0000

    implemented a basic version of availabilities

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   get 'meals/upcoming_meals', to: 'meals#upcoming_meals', as: 'upcoming_meals'
   get 'meals/past_meals', to: 'meals#past_meals', as: 'past_meals'
   get 'meals/show', to: 'meals#show', as: 'meal'
-  get 'availabilities/index', to: 'availabilities#index', as: 'availabilities'
+
+  get 'availabilities', to: 'availabilities#index', as: 'availabilities'
+  post 'availabilities', to: 'availabilities#create'
+  delete 'availability/:id', to: 'availabilities#destroy', as: 'availability_delete'
   get 'interests/index', to: 'interests#index', as: 'interests'
   get 'interests/edit', to: 'interests#edit', as: 'interests_edit'
-  get 'users/dashboard', to: 'users#past_meals', as: 'dashboard'
+  get 'users/dashboard', to: 'users#dashboard', as: 'dashboard'
   get 'users/edit_profile', to: 'users#edit_profile', as: 'edit_profile'
   devise_for :users
   root to: 'pages#home'
