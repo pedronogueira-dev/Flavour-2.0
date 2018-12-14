e4a1f042416933ff9720f704554a18f5414048f9
Pedro Nogueira <17pedro.n@gmail.com>
2018-12-14 17:05:43 +0000

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -197,7 +197,7 @@ puts "Created #{User.count} New Users"
 
 puts "----------------Seeding meals--------------------"
 
-users.all.each do |user|
+Users.all.each do |user|
   Availability.create!(date: "2018-12-4")
   Availability.create!(date: "2018-12-20")
   Availability.create!(date: "2018-12-24")
