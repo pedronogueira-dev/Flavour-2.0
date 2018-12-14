33539b664adbc7ea98db8404f45d784aa9cc6b7a
Thomas Singell <tsingell@gmail.com>
2018-12-14 15:56:17 +0000

    linked active meal index page to show meal page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'meals/upcoming_meals', to: 'meals#upcoming_meals', as: 'upcoming_meals'
   get 'meals/past_meals', to: 'meals#past_meals', as: 'past_meals'
-  get 'meals/show', to: 'meals#show', as: 'meal'
+  get 'meals/:id', to: 'meals#show', as: 'meal'
   get 'availabilities/index', to: 'availabilities#index', as: 'availabilities'
   get 'user_interests/index', to: 'user_interests#index', as: 'interests'
   get 'user_interests/edit', to: 'user_interests#edit', as: 'interests_edit'
