e99ac9256d00835b98442a20a068cb6875916d9e
Pedro Nogueira <17pedro.n@gmail.com>
2018-12-14 06:09:19 +0000

    added flatpickr to package.json

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,8 @@
 import "bootstrap";
 import { addEventListenersToInterests } from '../components/interests';
 
+import flatpickr from "flatpickr"
+import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+
 addEventListenersToInterests();
+flatpickr(".datepicker", {})
