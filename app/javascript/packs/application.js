ed6cf79ad54e18d77cfc2e45e715d6c186a5100b
Jami Owen <jamirene88@gmail.com>
2018-12-17 15:17:37 +0000

    made active status follow active pages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,8 @@ import "bootstrap";
 import { addEventListenersToInterests } from '../components/interests';
 // import { picker } from '../plugins/datepicker';
 import { calendarListener } from '../components/calendar';
-import { addActiveStatusToTab } from '../components/tab_active';
 
 addEventListenersToInterests();
 
 // picker();
 calendarListener();
-addActiveStatusToTab();
