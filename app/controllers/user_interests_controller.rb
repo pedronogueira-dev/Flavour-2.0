3fb0a07df16e03418acae121433b17c5e20fb416
Jami Owen <jamirene88@gmail.com>
2018-12-20 11:11:16 +0000

    fixed update interest problems

--- app/controllers/user_interests_controller.rb
+++ app/controllers/user_interests_controller.rb
@@ -8,18 +8,20 @@ class UserInterestsController < ApplicationController
   end
 
   def update
-    @interest = current_user.user_interests.find(params[:id])
-    if @interest.update(user_interests_params)
-      respond_to do |format|
-        format.html { redirect_to interests_edit_path }
-        format.js # <-- will render `app/views/user_interest/update.js.erb`
+    #@interest = current_user.user_interests.find(params[:id])
+   @interest = UserInterest.find_by(user: current_user, id: params[:id])
+    if @interest
+      if @interest.update(user_interests_params)
+        respond_to do |format|
+          format.html { redirect_to interests_edit_path }
+          format.js { head :ok } # <-- will render `app/views/user_interest/update.js.erb`
+        end
+      else
+        respond_to do |format|
+          format.html { render :edit }
+          format.js { head :ok }  # <-- idem
+        end
       end
-    else
-      respond_to do |format|
-        format.html { render :new }
-        format.js  # <-- idem
-      end
-
     end
   end
 
