abe3e94852ff832e86d0cb4045676e9835b2ef3e
Pedro Nogueira <17pedro.n@gmail.com>
2018-12-20 13:30:58 +0000

    added meta-info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     dashboard_path
   end
+
+  def default_url_options
+  { host: ENV["HOST"] || "localhost:3000" }
+  end
 end
